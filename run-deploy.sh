2fe44c95aeef5e518d3ae0159edb8ae1144d08fc
Dan Tormin <mintz.dnt@gmail.com>
2025-03-12 17:49:44 +0500

    small fix

--- run-deploy.sh
+++ run-deploy.sh
@@ -3,7 +3,10 @@
 git clone https://github.com/DanyMint/athena-backend;
 git clone https://github.com/DanyMint/athena-frontend;
 
-chmod +x ./athena-frontend/install.sh;
-sudo ./athena-frontend/install.sh;
+cd ./athena-frontend
+chmod +x ./install.sh;
+sudo ./install.sh;
 
-sudo docker-compose up --build -d
+
+cd ..
+sudo docker compose up --build -d
